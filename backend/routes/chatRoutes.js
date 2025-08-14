4f80639ac781915dc2b971bd3e3a09f3a91e0433
JackMocho <196417405+JackMocho@users.noreply.github.com>
2025-08-14 22:07:46 +0300

    ddddd

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const db = require('../config/db');
 const { logError } = require('../utils/logger');
 const { verifyToken } = require('../utils/jwtUtils');
 const router = express.Router();
